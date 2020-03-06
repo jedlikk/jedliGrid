14a7c045f1c3f12909b9e1b211e9e246693c12fd
Kacper Kaczmarek <kapel125@gmail.com>
2020-03-06 20:20:00 +0100

    [feature] Added functionality to generate rows and cols. Added functionality to delete rows

--- demo.js
+++ demo.js
@@ -5,6 +5,15 @@ const grid = document.querySelector("[data-item='grid']");
 const initGrid = new jedliGrid(grid);
 
 grid.addEventListener('afterInit', () => {
-    console.log("After init");
-    initGrid.addRow(1, 'head', {head1: "Nagłowek tekst 1", head2: "Muszka Gruszka"})
+    initGrid.addRow(1, ['head'], { customParam1: "Example 1", customParam2: "Example 2" })
+    initGrid.addRow(3, ['body'], { customParam1: "Example 1", customParam2: "Example 2" })
+    for (let i = 0; i < 3; i++) {
+        initGrid.addCol(1, ['body'], i, 'col 1', { customParam1: "Example 1", customParam2: "Example 2" })
+        initGrid.addCol(1, ['body'], i, 'col 2', { customParam1: "Example 1", customParam2: "Example 2" })
+    }
+})
+
+const buttonDelete = document.querySelector("[data-action='delete-row']");
+buttonDelete.addEventListener('click', () => {
+    initGrid.deleteRow(0, ['body']);
 })
--- jedligrid.js
+++ jedligrid.js
@@ -5,7 +5,7 @@
 // v 1.0.0
 
 class jedliGrid {
-    constructor (item, options) {
+    constructor(item, options) {
         // Set item (container) as part of class
         this.item = item;
 
@@ -30,10 +30,10 @@ class jedliGrid {
             // Object with options
             options: this.options,
             // last and next index of col
-            nextIndexCol: 0,
-            lastIndexCol: -1,
-            nextIndexRow: 0,
-            lastIndexRow: -1,
+            nextIndexRowHead: 0,
+            lastIndexRowHead: -1,
+            nextIndexRowBody: 0,
+            lastIndexRowBody: -1,
         }
 
         // Set this.structure to contain whole structure
@@ -55,23 +55,22 @@ class jedliGrid {
             this.initHead();
             this.initBody();
         })
-        .then(() => {
-            // Append whole structure to DOM
-            this.initDOM();
-            console.log(this);
+            .then(() => {
+                // Append whole structure to DOM
+                this.initDOM();
 
-            // Trigger event after init
-            this.eventAfterInit();
-        });
+                // Trigger event after init
+                this.eventAfterInit();
+            });
     }
 
     // Init root of grid
     async initRoot() {
         let root = document.createElement("table");
         // Add attr and classes
-        root.setAttribute("data-jedli", "root");
+        root.setAttribute("data-jedli-grid", "root");
         root.classList.add("jedli-grid");
-        
+
         // Add to structure 
         this.structure.root = root;
     }
@@ -80,9 +79,9 @@ class jedliGrid {
     async initHead() {
         let head = document.createElement("thead");
         // Add attr and classes
-        head.setAttribute("data-jedli", "head");
+        head.setAttribute("data-jedli-grid", "head");
         head.classList.add("jedli-grid__head");
-        
+
         // Add to structure 
         this.structure.head = head;
 
@@ -94,9 +93,9 @@ class jedliGrid {
     async initBody() {
         let body = document.createElement("tbody");
         // Add attr and classes
-        body.setAttribute("data-jedli", "body");
+        body.setAttribute("data-jedli-grid", "body");
         body.classList.add("jedli-grid__body");
-        
+
         // Add to structure 
         this.structure.body = body;
 
@@ -122,26 +121,82 @@ class jedliGrid {
     // Type = 'string'
     // Defines which type of index will be added
     // Index of cols or index of rows
-    async setIndex(element, type) {
+
+    // target = string
+    // Defines which index should be used, index for head or for body
+
+    // Parent = element
+    // Used for columns, allows to find last index in row
+    async setIndex(element, type, target, parent) {
         // Get right index to add
         // And update old one
         let indexToAdd;
-        console.log(type);
-        switch(type) {
+        switch (type) {
             case 'col':
-                indexToAdd = this.state.nextIndexCol;
-                this.state.nextIndexCol++;
-                this.state.lastIndexCol++;
+                // In col find last col and set index as index of last col + 1
+                // Get all cols
+                let lastCol = parent.querySelectorAll("[data-jedli-grid='col']");
+                // Check if there is any col
+                if (lastCol.length > 0) {
+                    // If true
+                    // Get last col
+                    lastCol = lastCol[lastCol.length - 1];
+                    // Get index of last col
+                    indexToAdd = +lastCol.getAttribute('jedli-grid-index');
+                    // Increment it
+                    indexToAdd++;
+
+                }
+                else {
+                    // If not, set index as 0
+                    indexToAdd = 0;
+                }
+
                 break;
+
             case 'row':
-                indexToAdd = this.state.nextIndexRow;
-                this.state.nextIndexRow++;
-                this.state.lastIndexRow++;
-                break;
+                // Use different indexes for body and head
+                switch (target) {
+                    case 'head':
+                        indexToAdd = this.state.nextIndexRowHead;
+                        this.state.nextIndexRowHead++;
+                        this.state.lastIndexRowHead++;
+                        break;
+
+                    case 'body':
+                        indexToAdd = this.state.nextIndexRowBody;
+                        this.state.nextIndexRowBody++;
+                        this.state.lastIndexRowBody++;
+                        break;
+                }
         }
-        
+
         // Add attr
-        element.setAttribute("jedli-index", indexToAdd);
+        element.setAttribute("jedli-grid-index", indexToAdd);
+    }
+
+    // @Params
+    // category = string
+    // Define what elements should have been updated
+
+    // target = array, can contain 'head', 'body' or both
+    // Specified where col be added, by default to both head and body
+    async updateIndex(category, target = []) {
+        // Loop through target and find all elements of this category in target
+        const keys = target.keys;
+        if (keys) {
+            target.forEach(singleTarget => {
+                // Get all elemnts of this category 
+                let allElements = this.structure[singleTarget].querySelectorAll(`[data-jedli-grid='${category}']`);
+                if (allElements.length > 0) {
+                    // If there are any elements, loop through
+                    allElements.forEach((element, index) => {
+                        // Set index of loop as index of element
+                        element.setAttribute("jedli-grid-index", index);
+                    })
+                }
+            });
+        }
     }
 
     // Add content to element in DOM
@@ -165,30 +220,57 @@ class jedliGrid {
 
     // Add column 
     // @Params:
+
+    // target = array of strings
+    // Specified where this row is, head or body
+
+    // rowIndex = int/string  (default 0)
+    // Specified index of row where col will be added
+
     // content = string/html
     // Contains what will be added to col
 
-    // target = array, can contain 'head', 'body' or both
-    // Specified where col be added, by default to both head and body
-    async addCol(content = "", target = ['head', 'body']) {
+    // customParams = object
+    // Allows to add custom params to specified row
+    async addCol(amount = 1, target = ['head', 'body'], rowIndex = 0, content = "", customParams = {}) {
         const keys = target.keys;
         // Loop through targets and add to all targets
-        if(keys) {
-            target.forEach(element => {
-                // Create html element for col
-                let col = document.createElement("td");;
-                
-                // Add specific attributes
-                col.setAttribute("data-jedli", "col");
-                this.setIndex(col, 'col');
-                col.classList.add("jedli-grid__col");
+        if (keys) {
+            // Loop whole adding function, depends of specified 'amount' number
+            for (let i = 0; i < amount; i++) {
 
-                // Add content to col
-                this.addContent(col, content);
-                
-                // Append to specified place in dom
-                this.structure[element].appendChild(col);
-            });
+                target.forEach(element => {
+                    // Find row with specified index
+                    let row = this.structure[element].querySelector(`[data-jedli-grid='row'][jedli-grid-index='${rowIndex}']`);
+                    if (row) {
+                        // Create html element for col
+                        let col = document.createElement("td");
+
+                        // Add specific attributes
+                        col.setAttribute("data-jedli-grid", "col");
+                        this.setIndex(col, 'col', element, row);
+                        col.classList.add("jedli-grid__col");
+
+                        // Add content to col
+                        this.addContent(col, content);
+
+                        // Loop through keys of custom params and add them to col
+                        let paramsKeys = Object.keys(customParams);
+                        if (paramsKeys.length > 0) {
+                            paramsKeys.forEach(key => {
+                                // Add param to row
+                                col.setAttribute(key, customParams[key]);
+                            });
+                        }
+
+                        // Append to specified place in dom
+                        row.appendChild(col);
+                    }
+                    else {
+                        console.error("jedliGrid ERROR! Row not found in function 'addCol()'")
+                    }
+                });
+            }
         }
     }
 
@@ -199,31 +281,99 @@ class jedliGrid {
 
     // target = array, can contain 'head', 'body' or both
     // Specified where col be added, by default to both head and body
-    async addRow(amount = 1, target = ['head', 'body'], customParams = ["custom-index", value]) {
+
+    // customParams = object
+    // Allows to add custom params to specified row
+    async addRow(amount = 1, target = ['head', 'body'], customParams = {}) {
         const keys = target.keys;
         // Loop through targets and add to all targets
-        if(keys) {
-            target.forEach(element => {
-                // Create html element for row
-                let row;
-                // If target element is 'head' then create th
-                
-                // Add specific attributes
-                row.setAttribute("data-jedli", "row");
-                this.setIndex(col);
-                row.classList.add("jedli-grid__row");
+        if (keys) {
+            // Loop whole adding function, depends of specified 'amount' number
+            for (let i = 0; i < amount; i++) {
+                target.forEach(element => {
+                    // Create html element for row
+                    let row = document.createElement('tr');
 
-                // Add content to col
-                this.addContent(row, content);
-                
-                // Append to specified place in dom
-                this.structure[element].appendChild(row);
-            });
+                    // Add specific attributes
+                    row.setAttribute("data-jedli-grid", "row");
+                    this.setIndex(row, 'row', element);
+                    row.classList.add("jedli-grid__row");
+
+                    // Loop through keys of custom params and add them to row
+                    let paramsKeys = Object.keys(customParams);
+                    if (paramsKeys.length > 0) {
+                        paramsKeys.forEach(key => {
+                            // Add param to row
+                            row.setAttribute(key, customParams[key]);
+                        });
+                    }
+
+                    // Append to specified place in dom
+                    this.structure[element].appendChild(row);
+                });
+            }
         }
     }
 
+    // Delete row
+    // @Params:
+    // index = int/string
+    // Index of row to delete
+
+    // target = array, can contain 'head', 'body' or both
+    // Specified where to find row, by default to both head and body
+
+    async deleteRow(index, target = []) {
+        this.deleteElement('row', index, target);
+    }
+
+    // Delete col
+    // @Params:
+    // index = int/string
+    // Index of col to delete
+
+    // target = array, can contain 'head', 'body' or both
+    // Specified where to find col, by default to both head and body
+
+    async deleteCol(index, target = []) {
+        this.deleteElement('col', index, target);
+    }
+
+    // Functon to delet element row/col
+    // @Params 
+    // type = string
+    // defines if should remove col or row
+
+    // index = int/string
+    // Index of element to delete
+
+    // target = array, can contain 'head', 'body' or both
+    // Specified where to find element, by default to both head and body
+
+    async deleteElement(category, index, indexOfParent, target = []) {
+        // Find right element in right target
+        const keys = target.keys;
+        // Loop through targets and add to all targets
+        if (keys) {
+            target.forEach(singleTarget => {
+                // Find right row
+                let element = this.structure[singleTarget].querySelector(`[data-jedli-grid='${category}'][jedli-grid-index='${index}']`);
+                // Check if row exists
+                if (element) {
+                    // If row has been found, remove it
+                    element.remove();
+
+                    // Update index of all rows that left
+                    this.updateIndex(category, [singleTarget]);
+                }
+                else {
+                    console.error("jedliGrid ERROR! Element not found in function 'deleteElement() (Function deleteElement() is used by deleteRow() and deleteCol()'")
+                }
+            });
+        }
+    }
     // EVENTS
-    
+
     eventAfterInit() {
         const eventAfterInit = new CustomEvent(
             'afterInit',
